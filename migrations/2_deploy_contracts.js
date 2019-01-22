ec88fd8448c56f0df21cec1697cfcb2051ba4aef
Adam Rodacki <amrodacki@gmail.com>
2019-01-22 13:01:29 +0100

    changed deadline to the future

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const Election = artifacts.require("Election");
 module.exports = function(deployer) {
     var voters = ['0x37c1aeEf65B00b0d2413E0530E71b159ad59D3A1', '0x77d0721Cd4e51175d8FfC2F95d17B230a2de83a9'];
     var candidates = ['Krzysztof Jarzyna', 'Twardydyskus Antywirus'];
-    var deadline = 2;
+    var deadline = 1648155328;
     var startTime = 1;
 
     deployer.deploy(Election, voters, candidates, deadline, startTime);
