a7e08cc567dccf6567b25ee7bef347cc2a0b29c7
Adam Rodacki <amrodacki@gmail.com>
2019-01-23 11:28:27 +0100

    added not-dynamic candidates table

--- client/src/ReadCandidates.js
+++ client/src/ReadCandidates.js
@@ -1,30 +1,52 @@
 import React from "react";
 
 class ReadCandidates extends React.Component {
-  state = { dataKey: null };
+  state = { candidateKeyList: [] };
 
   componentDidMount() {
     const { drizzle } = this.props;
 
     const contract = drizzle.contracts.Election;
 
-    // let drizzle know we want to watch the `candidate` method
-    const dataKey = contract.methods["candidates"].cacheCall(0);
-
-    // save the `dataKey` to local component state for later reference
-    this.setState({ dataKey });
+    contract.methods.candidatesCount().call().then((candidatesCount) => {
+      var candidateKeyList = [];
+      for (var i = 0; i < candidatesCount; i++) {
+        const candidateKey = contract.methods["candidates"].cacheCall(i);
+        candidateKeyList.push(candidateKey);
+      }
+      this.setState({ candidateKeyList });
+    });
   }
 
   render() {
-    // get the contract state from drizzleState
     const { Election } = this.props.drizzleState.contracts;
 
-    // using the saved `dataKey`, get the variable we're interested in
-    const candidate = Election.candidates[this.state.dataKey];
-
+    const candidatesCount = this.state.candidateKeyList.length;
+    var candidates = [];
+    for (var i = 0; i < candidatesCount; i++) {
+      candidates.push(Election.candidates[this.state.candidateKeyList[i]]);
+    }
+     
     return (
       <React.Fragment>
-        <p>Candidate: {candidate && candidate.value[1]}</p>
+      <table align="center">
+        <thead align="left">
+          <tr>
+            <th>Candidate</th>
+            <th>Votes</th>
+          </tr>
+        </thead>
+        <tbody align="left">
+          <tr>
+            <td>{candidates[0] && candidates[0].value[1]}</td>
+            <td>{candidates[0] && candidates[0].value[2]}</td>
+          </tr>
+          <tr>
+            <td>{candidates[1] && candidates[1].value[1]}</td>
+            <td>{candidates[1] && candidates[1].value[2]}</td>
+          </tr>
+        </tbody>
+      </table>
       </React.Fragment>
     );
   }
