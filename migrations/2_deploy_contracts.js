c07a4e809b1eb607dc5b61dc90bccc8e1a02e439
Adam Rodacki <amrodacki@gmail.com>
2019-01-23 22:09:54 +0100

    changed constructor parameters to more real :)

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 const Election = artifacts.require("Election");
 
 module.exports = function(deployer) {
-    var voters = ['0x37c1aeEf65B00b0d2413E0530E71b159ad59D3A1', '0x77d0721Cd4e51175d8FfC2F95d17B230a2de83a9'];
-    var candidates = ['Krzysztof Jarzyna', 'Twardydyskus Antywirus'];
-    var deadline = 1648155328;
-    var startTime = 1;
+    var voters = ['0x3F340CD22C04478A5D079d564a96Df28a5C8A78E', '0x97bb0ec7001632aD8Bb6D9551492559f5F5f09E1'];
+    var candidates = ['Jan Kowalski', 'Anna Wiśniewska'];
+    var deadline = 1548673200;
+    var startTime = 1547982000;
 
     deployer.deploy(Election, voters, candidates, deadline, startTime);
 };
